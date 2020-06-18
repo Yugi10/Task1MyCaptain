c7aec165743ddc97723e7d8895f21d2d0e2d1bb3
Yugi10 <67090447+Yugi10@users.noreply.github.com>
2020-06-18 11:51:51 +0530

    Task1 of MyCaptain python course uploaded
    The programs are :
    1) To find the area of the circle of given radius
    2) To find the extension of a given file name

---
+++ AreaCircle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+from math import pi
+r = float(input ("Please enter the Radius of your Circle:\n"))
+A = (pi * r * r)
+C = (2*pi*r)
+print ("\nThe Area of the Circle with Radius " + str(r) + " is:\n",A)
+print ("\nThe Circumference of the Circle with Radius " + str(r) + " is:\n",C)
+
+
+# In[1]:
+
+
+from math import pi
+r = float(input ("Please enter the Radius of your Circle:\n"))
+A = (pi * r * r)
+C = (2*pi*r)
+print ("\nThe Area of the Circle with Radius " + str(r) + " is:\n",A)
+print ("\nThe Circumference of the Circle with Radius " + str(r) + " is:\n",C)
+
+
+# In[2]:
+
+
+from math import pi
+r = float(input ("Please enter the Radius of your Circle:\n"))
+A = (pi * r * r)
+C = (2*pi*r)
+print ("\nThe Area of the Circle with Radius " + str(r) + " is:\n",A)
+print ("\nThe Circumference of the Circle with Radius " + str(r) + " is:\n",C)
+
---
+++ FileNameExtension.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import os
+filename = input ("Please enter the name of your file with extension :\n")
+extn = os.path.splitext (filename)
+print ("The file name without extension is :\n" , extn[0])
+print ("The extension of file name " + str (filename) + " is :\n" , extn[1])
+
+
+# In[7]:
+
+
+import os
+filename = input ("Please enter the name of your file with extension :\n")
+extn = os.path.splitext (filename)
+print ("The file name without extension is :\n" , extn[0])
+print ("The extension of file name " + str (filename) + " is :\n" , extn[1])
+
+
+# In[8]:
+
+
+import os
+filename = input ("Please enter the name of your file with extension :\n")
+extn = os.path.splitext (filename)
+print ("The file name without extension is :\n" , extn[0])
+print ("The extension of file name " + str (filename) + " is :\n" , extn[1])
+
+
+# In[9]:
+
+
+import os
+filename = input ("Please enter the name of your file with extension :\n")
+extn = os.path.splitext (filename)
+print ("The file name without extension is :\n" , extn[0])
+print ("The extension of file name " + str (filename) + " is :\n" , extn[1])
+
