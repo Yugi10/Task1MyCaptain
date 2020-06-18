cce0daefd906079b1bf9d507584190b82b8715f6
Yugi10 <67090447+Yugi10@users.noreply.github.com>
2020-06-18 12:04:30 +0530

    Update FileNameExtension.py

--- FileNameExtension.py
+++ FileNameExtension.py
@@ -1,5 +1,5 @@
 import os
 filename = input ("Please enter the name of your file with extension :\n")
 extn = os.path.splitext (filename)
-print ("The file name without extension is :\n" , extn[0])
-print ("The extension of file name " + str (filename) + " is :\n" , extn[1])
+print ("\nThe file name without extension is :\n" , extn[0])
+print ("\nThe extension of file name " + str (filename) + " is :\n" , extn[1])
