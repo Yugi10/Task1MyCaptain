3bf4445aea0c8274a72f55f9ef698a2274390721
Yugi10 <67090447+Yugi10@users.noreply.github.com>
2020-06-19 09:41:56 +0530

    Update AreaCircle.py

--- AreaCircle.py
+++ AreaCircle.py
@@ -4,3 +4,12 @@ A = (pi * r * r)
 C = (2*pi*r)
 print ("\nThe Area of the Circle with Radius " + str(r) + " is:\n",A)
 print ("\nThe Circumference of the Circle with Radius " + str(r) + " is:\n",C)
+
+Please enter the Radius of your Circle:
+2.5
+
+The Area of the Circle with Radius 2.5 is:
+ 19.634954084936208
+
+The Circumference of the Circle with Radius 2.5 is:
+ 15.707963267948966
