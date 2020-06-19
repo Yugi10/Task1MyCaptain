5e441b6220a81532e6759e50767e9334ba77262e
Yugi10 <67090447+Yugi10@users.noreply.github.com>
2020-06-19 09:50:19 +0530

    Update FileNameExtension.py

--- FileNameExtension.py
+++ FileNameExtension.py
@@ -4,6 +4,9 @@ extn = os.path.splitext (filename)
 print ("\nThe file name without extension is :\n" , extn[0])
 print ("\nThe extension of file name " + str (filename) + " is :\n" , extn[1])
 
+"""
+OUTPUT :
+
 Please enter the name of your file with extension :
 yugi.py
 
@@ -13,3 +16,4 @@ The file name without extension is :
 The extension of file name yugi.py is :
  .py
 
+"""
